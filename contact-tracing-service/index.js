20dc78c7c31b119ec9b74e2dc261440a69ae31a2
Sebastian Kargl <kargl121@gmail.com>
2022-01-13 14:53:54 +0100

    added npm run start for express with nodemon

--- contact-tracing-service/index.js
+++ contact-tracing-service/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var port = 8000;
+
 app.get('/', function (req, res) {
     res.send("Hello");
 });
+
 app.listen(port);
--- contact-tracing-service/index.ts
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port: number = 8000;
-
-app.get('/', (req, res)=>{
-    res.send("Hello")
-})
-
-app.listen(port)
