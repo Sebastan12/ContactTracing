97b2ef0590cdc49fe2acc2f35746588065e95c57
Sebastian Kargl <kargl121@gmail.com>
2022-01-26 00:18:45 +0100

    Navigation is now working. Register Login and Navbar visuals are finished

--- contact-tracing/src/app/app.module.ts
+++ contact-tracing/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
