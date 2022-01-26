a5b1942c028da96c3051fa85845ecdb151bd3789
Sebastian Kargl <kargl121@gmail.com>
2022-01-26 01:57:08 +0100

    display username upon login

--- contact-tracing/src/app/home/home.component.ts
+++ contact-tracing/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  message:string = "You are not logged in";
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get('http://localhost:8000/api/user', {withCredentials: true})
+      .subscribe((res:any) => {
+        console.log(res);
+        this.message = `Hi ${res.username}`;
+      },
+          err => {
+            console.log(err);
+            this.message = "You are not logged in";
+        })
   }
 
 }
